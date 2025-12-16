33e97861669fb9d9411ea1e30495da65d5ee95af
ricknaldos <ricardo@rowship.com>
2025-12-16 00:30:22 -0500

    feat: add scroll position tracking to Rowship feedback script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,10 @@ export default function RootLayout({
     <html lang="es">
       <body>
         {children}
-        {/* Rowship Feedback Tracker - URL detection + scroll-to-position in iframe */}
+        {/* Rowship Feedback Tracker - URL + scroll position detection + scroll-to in iframe */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(){if(window.parent!==window){var s=function(){window.parent.postMessage({type:'ROWSHIP_URL_CHANGE',url:location.href},'*')};s();var p=history.pushState;history.pushState=function(){p.apply(this,arguments);s()};var r=history.replaceState;history.replaceState=function(){r.apply(this,arguments);s()};addEventListener('popstate',s);addEventListener('hashchange',s);addEventListener('message',function(e){if(e.data&&e.data.type==='ROWSHIP_SCROLL_TO'){window.scrollTo({top:e.data.scrollY||0,left:e.data.scrollX||0,behavior:'smooth'})}})}})();`
+            __html: `(function(){if(window.parent!==window){var s=function(){window.parent.postMessage({type:'ROWSHIP_URL_CHANGE',url:location.href,scrollY:window.scrollY||document.documentElement.scrollTop||0},'*')};s();var p=history.pushState;history.pushState=function(){p.apply(this,arguments);s()};var r=history.replaceState;history.replaceState=function(){r.apply(this,arguments);s()};addEventListener('popstate',s);addEventListener('hashchange',s);addEventListener('scroll',function(){window.parent.postMessage({type:'ROWSHIP_SCROLL_CHANGE',scrollY:window.scrollY||document.documentElement.scrollTop||0},'*')});addEventListener('message',function(e){if(e.data&&e.data.type==='ROWSHIP_SCROLL_TO'){window.scrollTo({top:e.data.scrollY||0,left:e.data.scrollX||0,behavior:'smooth'})}})}})();`
           }}
         />
       </body>
